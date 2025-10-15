ab8644d0a945d37860c1e4b56e60b8657ebef6fa
김도영 <zz01064842675@gmail.com>
2025-10-15 17:46:26 +0900

    feat: 기본 구분자(, :)로 분리하여 합산

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,7 +2,14 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
+    private static final String[] DEFAULT_DELIMITERS = {",", ":"};
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
@@ -11,5 +18,25 @@ public class Application {
         if (input == null || input.isEmpty()) {
             System.out.println("결과 : 0");
         }
+
+        List<Integer> result = new ArrayList<>();
+
+        String splitRegex = Arrays.stream(DEFAULT_DELIMITERS).collect(Collectors.joining("|"));
+        String[] tokens = input.split(splitRegex, 0);
+        for (String token : tokens) {
+            String s = token.trim();
+            if (s.isEmpty()) {
+                continue;
+            }
+
+            result.add(Integer.parseInt(s));
+        }
+
+        int acc = 0;
+        for (int n : result) {
+            acc += n;
+        }
+
+        System.out.println("결과 : " + acc);
     }
 }
